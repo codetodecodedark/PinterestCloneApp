7f87c6aec58f51b5c5718a6dd79bfa59c463e0ad
codetodecodedark <niteshgarssa19@gmail.com>
2024-04-26 12:06:48 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./posts");
+const passport = require('passport');
+
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
+
+
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login', { error: req.flash('error') });
+});
+
+router.get('/feed', function (req, res, next) {
+  res.render('feed');
+});
+
+router.get('/profile', isLoggedIn, async function (req, res, next) {
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  })
+  res.render("profile", { user });
+});
+
+router.post("/register", function (req, res) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, email, fullname });
+
+  userModel.register(userData, req.body.password)
+    .then(function () {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/profile");
+      })
+    })
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/login",
+  failureFlash: true
+}), function (req, res) {
+});
+
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+
+}
+
+module.exports = router;
